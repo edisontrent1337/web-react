053d52cb4e86820125d79e4fb0d6fb10b7a191c3
Sinthujan Thanabalasingam (DevBoost) <sthana@devbooost.de>
2018-11-15 19:00:47 +0100

    Added Hint.js

---
+++ hints/Hint.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./hint.fx.css";
+import Button from "../button/Button";
+
+export default class Hint extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="hintContainer">Nothing here.
+			<Button/></div>
+		);
+	}
+
+}
