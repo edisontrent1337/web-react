2390daebc7cf4be3b651bef0f7ef5dd82f61de41
Sinthujan Thanabalasingam (DevBoost) <sthana@devboost.de>
2018-12-14 17:08:39 +0100

    Added dismissable alerts.

--- message/Message.js
+++ message/Message.js
@@ -3,14 +3,38 @@ import {Alert} from "react-bootstrap";
 
 export default class Message extends React.Component {
 
+	constructor(props, context) {
+		super(props, context);
+
+		this.handleDismiss = this.handleDismiss.bind(this);
+		this.handleShow = this.handleShow.bind(this);
+
+		this.state = {
+			show: true
+		};
+	}
+
+	handleDismiss() {
+		this.setState({show: false});
+	}
+
+	handleShow() {
+		this.setState({show: true});
+	}
 
 	render() {
-		return (
-			<Alert bsStyle={this.props.bsStyle}>
-				<i className="fas fa-exclamation-triangle" style={{marginRight: "6px"}}></i>{this.props.heading}
-				<p>{this.props.message}</p>
-			</Alert>
-		);
+		if (this.state.show) {
+			const {dismissable} = this.props;
+			return (
+				<Alert bsStyle={this.props.bsStyle} onDismiss={dismissable ? this.handleDismiss : undefined}>
+					<i className="fas fa-exclamation-triangle" style={{marginRight: "6px"}}></i>{this.props.heading}
+					{this.props.message}
+				</Alert>
+			);
+		}
+		else {
+			return <span></span>;
+		}
 	}
 
 }
