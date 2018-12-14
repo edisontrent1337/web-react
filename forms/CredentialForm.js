93a876583256b55b5837605767190238eddb60a9
Sinthujan Thanabalasingam (DevBoost) <sthana@devboost.de>
2018-12-14 14:43:28 +0100

    Added trigger to process form on press of enter button.

--- forms/CredentialForm.js
+++ forms/CredentialForm.js
@@ -10,115 +10,132 @@ export default class CredentialForm extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            mounted: false,
+            error: undefined
+        };
     }
 
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value
         });
+        this.setState({
+            error: undefined
+        });
+    }
+
+    componentDidMount() {
+        this.setState({mounted: true});
     }
 
     render() {
 
-
-        const inputs = this.props.inputs.map((input, i) => {
-            let {label, id, type, value, handler, hint, validator, mode, color, triggerOnEnter} = input;
-            if (typeof  mode === 'undefined')
-                mode = 'regular';
-            if (type === "text" || type === "password") {
-                return (
-                    <InputField
-                        color={this.props.color}
-                        label={label}
-                        key={i}
-                        id={id}
-                        name={id}
-                        type={type}
-                        onChange={this.props.onChange}
-                        hint={hint}
-                        onBlur={triggerOnEnter ? handler : null}
-                    />
-                );
-            } else if (type === "button") {
-                return (
-                    <Button
-                        color={this.props.color || color}
-                        onClick={handler}
-                        mode={mode}
-                        key={i}
-                        value={value}
-                        hint={hint}
-                        validator={validator}
-                    />
-                );
-            }
-            else if (type === "textarea") {
-                return (
-                    <TextArea
-                        color={this.props.color}
-                        label={label}
-                        key={i}
-                        id={id}
-                        name={id}
-                        onChange={this.props.onChange}
-                        hint={hint}
-                    />
-                );
-            }
-        });
-
-        let links = <div></div>;
-        if (typeof this.props.links !== 'undefined') {
-            links = this.props.links.map((link, i) => {
-                return (
-                    <div key={i}>
-                        <a style={{fontSize: "14px"}} href={link.href}>{link.value}</a>
-                    </div>
-                );
+        if (!this.state.mounted) {
+            return "Loading"
+        }
+        else {
+            const inputs = this.props.inputs.map((input, i) => {
+                let {label, id, type, value, handler, hint, validator, mode, color, triggerOnEnter} = input;
+                if (typeof  mode === 'undefined')
+                    mode = 'regular';
+                if (type === "text" || type === "password") {
+                    return (
+                        <InputField
+                            color={this.props.color}
+                            label={label}
+                            key={i}
+                            id={id}
+                            name={id}
+                            type={type}
+                            onChange={this.props.onChange}
+                            hint={hint}
+                            handler={handler}
+                        />
+                    );
+                } else if (type === "button") {
+                    return (
+                        <Button
+                            color={this.props.color || color}
+                            onClick={handler}
+                            mode={mode}
+                            key={i}
+                            value={value}
+                            hint={hint}
+                            validator={validator}
+                        />
+                    );
+                }
+                else if (type === "textarea") {
+                    return (
+                        <TextArea
+                            color={this.props.color}
+                            label={label}
+                            key={i}
+                            id={id}
+                            name={id}
+                            onChange={this.props.onChange}
+                            hint={hint}
+                        />
+                    );
+                }
             });
-        }
 
-        let {title, error, logo} = this.props;
-        if (typeof logo === "undefined") {
-            logo = <BimbayLogo/>;
-        }
+            let links = <div></div>;
+            if (typeof this.props.links !== 'undefined') {
+                links = this.props.links.map((link, i) => {
+                    return (
+                        <div key={i}>
+                            <a style={{fontSize: "14px"}} href={link.href}>{link.value}</a>
+                        </div>
+                    );
+                });
+            }
 
-        return (
-            <div
-                style={{
-                    backgroundColor: "#FFF",
-                    padding: "30px",
-                    borderRadius: "6px",
-                    border: "1px solid " + colors.blueGrey["100"]
-                }}
-            >
-                {title && <h4
-                    className="text-center"
+            let {title, error, logo} = this.props;
+            if (typeof logo === "undefined") {
+                logo = <BimbayLogo/>;
+            }
+
+
+
+            return (
+                <div
                     style={{
-                        color: "#012",
-                        fontSize: "30px"
+                        backgroundColor: "#FFF",
+                        padding: "30px",
+                        borderRadius: "6px",
+                        border: "1px solid " + colors.blueGrey["100"]
                     }}
                 >
-                    <div style={{marginBottom: "20px"}}>
-                        {logo}
+                    {title && <h4
+                        className="text-center"
+                        style={{
+                            color: "#012",
+                            fontSize: "30px"
+                        }}
+                    >
+                        <div style={{marginBottom: "20px"}}>
+                            {logo}
+                        </div>
+
+                        {title}
+                    </h4>
+                    }
+                    {title && <hr/>}
+
+                    <div>
+                        {error ? (
+                            <Alert bsStyle="danger">
+                                {error}
+                            </Alert>
+                        ) : null}
                     </div>
-
-                    {title}
-                </h4>
-                }
-                {title && <hr/>}
-
-                <div>
-                    {error ? (
-                        <Alert bsStyle="danger">
-                            {this.props.error}
-                        </Alert>
-                    ) : null}
+                    {inputs}
+                    <div style={{clear: "both"}}></div>
+                    {links}
                 </div>
-                {inputs}
-                <div style={{clear: "both"}}></div>
-                {links}
-            </div>
-        );
+            );
+        }
     }
 }
--- input/InputField.js
+++ input/InputField.js
@@ -4,55 +4,73 @@ import colors from "../colors/colors";
 
 export default class InputField extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            clicked: false
-        };
-        this.onFocusHandler = this.onFocusHandler.bind(this);
-        this.onFocusOutHandler = this.onFocusOutHandler.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			clicked: false,
+			mounted: false
+		};
+		this.onFocusHandler = this.onFocusHandler.bind(this);
+		this.onFocusOutHandler = this.onFocusOutHandler.bind(this);
+	}
 
-    onFocusHandler() {
-        this.setState({
-            clicked: true
-        })
-    }
+	componentDidMount() {
+		this.setState({
+			mounted: true
+		});
+	}
 
-    onFocusOutHandler() {
-        this.setState({
-            clicked: false
-        });
-        this.props.onBlur();
-    }
+	onFocusHandler() {
+		this.setState({
+			clicked: true
+		})
+	}
 
-    render() {
-        const {name, value, type, id, onChange, hint, placeholder} = this.props;
-        const formattedHint = hint && hint.map((elem, i) => {
-            return (<span key={i}>{elem}</span>);
-        });
-        return (
-            <div style={{margin: "0px 0px 15px 0px"}}>
-                {this.props.label && <label style={{
-                    color: colors.grey["700"],
-                    fontSize: "12px",
-                    fontWeight: "bold"
-                }}>
-                    {this.props.label}
-                </label>
-                }
-                <input type={type} id={id} style={{
-                    backgroundColor: (this.state.clicked ? "#FFF" : "#f2f2f2"),
-                }}
-                       onFocus={this.onFocusHandler}
-                       onBlur={this.onFocusOutHandler}
-                       value={value}
-                       onChange={onChange}
-                       name={name}
-                       placeholder={placeholder}
-                />
-                <span style={{fontSize: "12px", color: "#616161"}}>{formattedHint}</span>
-            </div>
-        );
-    }
+	onFocusOutHandler() {
+		this.setState({
+			clicked: false
+		});
+	}
+
+	render() {
+		if (!this.state.mounted) {
+			return "Loading";
+		}
+		else {
+			const {name, value, type, id, onChange, hint, placeholder, handler} = this.props;
+			const formattedHint = hint && hint.map((elem, i) => {
+				return (<span key={i}>{elem}</span>);
+			});
+			return (
+				<div style={{margin: "0px 0px 15px 0px"}}>
+					{this.props.label && <label style={{
+						color: colors.grey["700"],
+						fontSize: "12px",
+						fontWeight: "bold"
+					}}>
+						{this.props.label}
+					</label>
+					}
+					<input type={type} id={id} style={{
+						backgroundColor: (this.state.clicked ? "#FFF" : "#f2f2f2"),
+					}}
+						   onFocus={this.onFocusHandler}
+						   onBlur={this.onFocusOutHandler}
+						   value={value}
+						   onChange={onChange}
+						   onKeyPress={(e) => {
+							   if (e.key === "Enter") {
+								   if (handler) {
+									   handler(e);
+								   }
+							   }
+						   }}
+						   name={name}
+						   placeholder={placeholder}
+					/>
+					<span style={{fontSize: "12px", color: "#616161"}}>{formattedHint}</span>
+				</div>
+			);
+		}
+	}
 }
