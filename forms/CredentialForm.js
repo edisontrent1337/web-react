afe8851e066c9d231a4a042c2cd325efabd7d67a
Sinthujan Thanabalasingam (DevBoost) <sthana@devboost.de>
2018-12-14 14:46:13 +0100

    Added flag for inputs that should be triggered by pressing enter.

--- forms/CredentialForm.js
+++ forms/CredentialForm.js
@@ -7,135 +7,133 @@ import TextArea from "../input/TextArea";
 import colors from "../colors/colors";
 
 export default class CredentialForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            mounted: false,
-            error: undefined
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {
+			mounted: false,
+			error: undefined
+		};
+	}
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-        this.setState({
-            error: undefined
-        });
-    }
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+		this.setState({
+			error: undefined
+		});
+	}
 
-    componentDidMount() {
-        this.setState({mounted: true});
-    }
+	componentDidMount() {
+		this.setState({mounted: true});
+	}
 
-    render() {
+	render() {
 
-        if (!this.state.mounted) {
-            return "Loading"
-        }
-        else {
-            const inputs = this.props.inputs.map((input, i) => {
-                let {label, id, type, value, handler, hint, validator, mode, color, triggerOnEnter} = input;
-                if (typeof  mode === 'undefined')
-                    mode = 'regular';
-                if (type === "text" || type === "password") {
-                    return (
-                        <InputField
-                            color={this.props.color}
-                            label={label}
-                            key={i}
-                            id={id}
-                            name={id}
-                            type={type}
-                            onChange={this.props.onChange}
-                            hint={hint}
-                            handler={handler}
-                        />
-                    );
-                } else if (type === "button") {
-                    return (
-                        <Button
-                            color={this.props.color || color}
-                            onClick={handler}
-                            mode={mode}
-                            key={i}
-                            value={value}
-                            hint={hint}
-                            validator={validator}
-                        />
-                    );
-                }
-                else if (type === "textarea") {
-                    return (
-                        <TextArea
-                            color={this.props.color}
-                            label={label}
-                            key={i}
-                            id={id}
-                            name={id}
-                            onChange={this.props.onChange}
-                            hint={hint}
-                        />
-                    );
-                }
-            });
+		if (!this.state.mounted) {
+			return "Loading"
+		}
+		else {
+			const inputs = this.props.inputs.map((input, i) => {
+				let {label, id, type, value, handler, hint, validator, mode, color, triggerOnEnter} = input;
+				if (typeof  mode === 'undefined')
+					mode = 'regular';
+				if (type === "text" || type === "password") {
+					return (
+						<InputField
+							color={this.props.color}
+							label={label}
+							key={i}
+							id={id}
+							name={id}
+							type={type}
+							onChange={this.props.onChange}
+							hint={hint}
+							handler={triggerOnEnter === true ? handler : undefined}
+						/>
+					);
+				} else if (type === "button") {
+					return (
+						<Button
+							color={this.props.color || color}
+							onClick={handler}
+							mode={mode}
+							key={i}
+							value={value}
+							hint={hint}
+							validator={validator}
+						/>
+					);
+				}
+				else if (type === "textarea") {
+					return (
+						<TextArea
+							color={this.props.color}
+							label={label}
+							key={i}
+							id={id}
+							name={id}
+							onChange={this.props.onChange}
+							hint={hint}
+						/>
+					);
+				}
+			});
 
-            let links = <div></div>;
-            if (typeof this.props.links !== 'undefined') {
-                links = this.props.links.map((link, i) => {
-                    return (
-                        <div key={i}>
-                            <a style={{fontSize: "14px"}} href={link.href}>{link.value}</a>
-                        </div>
-                    );
-                });
-            }
+			let links= "";
+			if (typeof this.props.links !== 'undefined') {
+				links = this.props.links.map((link, i) => {
+					return (
+						<div key={i}>
+							<a style={{fontSize: "14px"}} href={link.href}>{link.value}</a>
+						</div>
+					);
+				});
+			}
 
-            let {title, error, logo} = this.props;
-            if (typeof logo === "undefined") {
-                logo = <BimbayLogo/>;
-            }
+			let {title, error, logo} = this.props;
+			if (typeof logo === "undefined") {
+				logo = <BimbayLogo/>;
+			}
 
+			return (
+				<div
+					style={{
+						backgroundColor: "#FFF",
+						padding: "30px",
+						borderRadius: "6px",
+						border: "1px solid " + colors.blueGrey["100"]
+					}}
+				>
+					{title && <h4
+						className="text-center"
+						style={{
+							color: "#012",
+							fontSize: "30px"
+						}}
+					>
+						<div style={{marginBottom: "20px"}}>
+							{logo}
+						</div>
 
+						{title}
+					</h4>
+					}
+					{title && <hr/>}
 
-            return (
-                <div
-                    style={{
-                        backgroundColor: "#FFF",
-                        padding: "30px",
-                        borderRadius: "6px",
-                        border: "1px solid " + colors.blueGrey["100"]
-                    }}
-                >
-                    {title && <h4
-                        className="text-center"
-                        style={{
-                            color: "#012",
-                            fontSize: "30px"
-                        }}
-                    >
-                        <div style={{marginBottom: "20px"}}>
-                            {logo}
-                        </div>
-
-                        {title}
-                    </h4>
-                    }
-                    {title && <hr/>}
-
-                    <div>
-                        {error ? (
-                            <Alert bsStyle="danger">
-                                {error}
-                            </Alert>
-                        ) : null}
-                    </div>
-                    {inputs}
-                    <div style={{clear: "both"}}></div>
-                    {links}
-                </div>
-            );
-        }
-    }
+					<div>
+						{error ? (
+							<Alert bsStyle="danger">
+								{error}
+							</Alert>
+						) : null}
+					</div>
+					{inputs}
+					<div style={{clear: "both"}}></div>
+					{links}
+				</div>
+			);
+		}
+	}
 }
