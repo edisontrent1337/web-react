972aa5233c718a79419359cb1bee7ca0328f9fcc
Sinthujan <sthana@devboost.de>
2019-01-21 13:32:58 +0100

    Fixed error on Circle rendering.

--- circle/Circle.js
+++ circle/Circle.js
@@ -3,50 +3,54 @@ import colors from "../colors/colors";
 
 export default class Circle extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.decideOnRandomColor = this.decideOnRandomColor.bind(this);
-		this.state = {
-			circleColor: colors.grey["50"]
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.decideOnRandomColor = this.decideOnRandomColor.bind(this);
+        this.state = {
+            backgroundColor: undefined,
+            fontColor: undefined
+        };
+    }
 
-	componentDidMount() {
-		this.decideOnRandomColor();
-	}
+    componentDidMount() {
+        this.decideOnRandomColor();
+    }
 
-	decideOnRandomColor() {
-		const numberOfColors = Object.keys(colors).length;
-		const randomIndex = Math.floor(Math.random() * (numberOfColors));
-		const color = Object.keys(colors)[randomIndex];
-		let result = colors[color]["50"];
-		if (typeof result === "undefined") {
-			result = colors.grey["50"];
-		}
-		this.setState({
-			circleColor: result
-		});
-	}
+    decideOnRandomColor() {
+        const numberOfColors = Object.keys(colors).length;
+        let randomIndex = Math.floor(Math.random() * (numberOfColors));
+        let color = Object.keys(colors)[randomIndex];
+        while (color.includes("black") || color.includes("white") || colors.includes("Icon") || colors.includes("Text")) {
+            randomIndex = Math.floor(Math.random() * (numberOfColors));
+            color = Object.keys(colors)[randomIndex];
+        }
+        let backgroundColor = colors[color]["100"];
+        let fontColor = colors[color]["400"];
+        this.setState({
+            backgroundColor: backgroundColor,
+            fontColor: fontColor
+        });
+    }
 
-	render() {
-		const {title, url} = this.props;
-		return (
-			<a href={url} style={{
-				borderRadius: "50%",
-				display: "inline-block",
-				width: "40px",
-				height: "40px",
-				backgroundColor: this.state.circleColor,
-				float: "left",
-				color: colors.grey["800"],
-				textDecoration: "none",
-				border: "1px solid " + colors.grey["50"]
-			}}>
-				<div style={{
-					textAlign: "center",
-					marginTop: "8px"
-				}}>{title[0].toUpperCase()}</div>
-			</a>
-		);
-	}
+    render() {
+        const {title, url} = this.props;
+        return (
+            <a href={url} style={{
+                borderRadius: "50%",
+                display: "inline-block",
+                width: "40px",
+                height: "40px",
+                backgroundColor: this.state.backgroundColor,
+                float: "left",
+                color: this.state.fontColor,
+                textDecoration: "none",
+                border: "1px solid " + colors.grey["50"]
+            }}>
+                <div style={{
+                    textAlign: "center",
+                    marginTop: "8px"
+                }}>{title}</div>
+            </a>
+        );
+    }
 }
