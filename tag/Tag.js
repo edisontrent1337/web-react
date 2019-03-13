4be2d656e5718a3f2e7ff4e75a9ceff99816a6b2
Sinthujan Thanabalasingam (DevBoost) <sthana@devbooost.de>
2019-03-13 23:44:18 +0100

    Added fontSize prop

--- tag/Tag.js
+++ tag/Tag.js
@@ -3,13 +3,13 @@ import React from "react";
 export default class Tag extends React.Component {
 
     render() {
-        const {padding} = this.props;
+        const {padding, fontSize} = this.props;
         return (
             <span style={{
                 border: "1px solid " + this.props.color,
                 borderRadius: "10px",
                 color: this.props.color,
-                fontSize: "10px",
+                fontSize: fontSize || "10px",
                 padding: padding || "5px",
                 fontWeight: "normal",
             }}>{this.props.tag}</span>
