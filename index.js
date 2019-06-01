6a88845ade67bd3c18af72594810e3d18aeaad92
Sinthujan Thanabalasingam (DevBoost) <sthana@devbooost.de>
2019-06-01 14:59:50 +0200

    Update to 0.2.19.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import Button from './lib/button';
 import InputField from './lib/input';
+import Form from './lib/forms';
+import Hint from './lib/hints';
 
 
 export {
   Button,
-  InputField
+  InputField,
+  Form,
+  Hint
 }
