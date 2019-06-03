4d4fbcbfdcfd4ea06c5c11722d0e8ac110564c95
Sinthujan Thanabalasingam (DevBoost) <sthana@devbooost.de>
2019-06-03 17:53:20 +0200

    Remove dependency to react-bootstrap's Alert from Message.

--- src/message/Message.js
+++ src/message/Message.js
@@ -1,43 +1,67 @@
 import React from "react";
-import {Alert} from "react-bootstrap";
+import colors from '../colors/colors.js';
 
 export default class Message extends React.Component {
 
 	constructor(props, context) {
 		super(props, context);
-
-		this.handleDismiss = this.handleDismiss.bind(this);
-		this.handleShow = this.handleShow.bind(this);
-
 		this.state = {
-			show: true
+			show: true,
+			hover: false
 		};
 	}
 
-	handleDismiss() {
+	handleDismiss = () => {
 		this.setState({show: false});
-	}
+	};
 
-	handleShow() {
+	handleShow = () => {
 		this.setState({show: true});
-	}
+	};
 
 	render() {
 		if (this.state.show) {
-			const {dismissable, icon} = this.props;
+			const {message, dismissIcon, icon, heading, color, width} = this.props;
 			return (
-				<Alert bsStyle={this.props.bsStyle} onDismiss={dismissable ? this.handleDismiss : undefined}>
-					<div style={{fontWeight: "bold"}}><i className={(icon ? icon : "fas fa-exclamation-triangle")}
-														 style={{marginRight: "6px"}}></i>
-						{this.props.heading}</div>
-					<div>
-						{this.props.message}
+				<div
+					style={{
+						padding: '10px 15px',
+						border: '1px solid ' + colors[color]['200'],
+						backgroundColor: colors[color]['100'],
+						color: colors[color]['900'],
+						margin: '10px',
+						borderRadius: '6px',
+						width: width ? width : 'auto'
+					}}>
+					<div style={{fontWeight: "bold"}}>
+
+						<div style={{display: 'table'}}>
+							{icon &&
+							<div
+								style={{display: 'table-cell', paddingRight: '8px', fontSize: '1.4em'}}
+								className={icon}/>}
+							<div style={{display: 'table-cell', width: '100%'}}>{heading}</div>
+							{dismissIcon &&
+							<a onMouseEnter={() => {
+								this.setState({hover: true})
+							}} onMouseLeave={() => {
+								this.setState({hover: false})
+							}} onClick={this.handleDismiss} style={{
+								display: 'table-cell',
+								fontSize: '1.4em',
+								cursor: this.state.hover ? 'pointer' : 'default'
+							}}
+							   className={dismissIcon}/>}
+						</div>
 					</div>
-				</Alert>
+					<div>
+						{message}
+					</div>
+				</div>
 			);
 		}
 		else {
-			return <span></span>;
+			return <span/>;
 		}
 	}
 
