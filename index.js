0485d42ed9029b238fa146668d50054e0ccfd951
Sinthujan Thanabalasingam (DevBoost) <sthana@devbooost.de>
2019-06-03 18:00:57 +0200

    Add index.js to Message and Hint

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import Button from './lib/button';
 import InputField from './lib/input';
 import Form from './lib/forms';
 import Hint from './lib/hints';
-
+import Message from './lib/message';
 
 export {
-  Button,
-  InputField,
-  Form,
-  Hint
+	Button,
+	InputField,
+	Form,
+	Hint,
+	Message
 }
